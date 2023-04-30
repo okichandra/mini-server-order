4cf1f6b46e192166297bd3e948a51e8f0e5e9abb
Oki Chandra Cristianta <setiadgnml@gmail.com>
2023-04-30 19:25:07 +0700

    desain harga

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,7 +3,13 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import data from './Data';
 
+function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
+}
+
 function GroupExample() {
+
+
     return (
         <div className="container mt-5">
             <h1 className='judul'>Makanan Yang Dapat Anda Pesan</h1>
@@ -14,7 +20,7 @@ function GroupExample() {
                             <Card.Img className='card-image' variant="top" src={list.gambar} />
                             <Card.Body>
                                 <Card.Title className='food-name'>{list.nama}</Card.Title>
-                                <Card.Text className='food-price'>Rp {list.harga}</Card.Text>
+                                <Card.Text className='food-price'>Rp {formatNumber(list.harga)}</Card.Text>
                                 <Card.Text className='food-rating'>
                                     <img width={20} src="https://img.icons8.com/fluency/48/null/star.png" alt="Rating image" />
                                     <span>{list.rating} | Terjual {list.terjual}</span>
